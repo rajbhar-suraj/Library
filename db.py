1a8a91bdcf39724aecac35c6823ff42587517a8d
rajbhar-suraj <suraj.rajbhar.tech@gmail.com>
2026-09-20 22:09:20 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,17 +3,11 @@ from psycopg2.extras import RealDictCursor
 
 def get_connection():
     return psycopg2.connect(
-        host="aws-0-ap-south-1.pooler.supabase.com",
-        # port=5432
-        database="postgres",
-        user="postgres.gitimlmcsskqsonrcjzg",
-        password="Shinichi00816",
+        host="",
+    
+        ",
+        ",
+        ",
         cursor_factory=RealDictCursor  # returns rows as dicts
     )
-
-
-# host=aws-0-ap-south-1.pooler.supabase.com
-# port=5432
-# database=postgres
-# user=postgres.gitimlmcsskqsonrcjzg
-#     DATABASE_URL=postgresql://postgres.gitimlmcsskqsonrcjzg:Shinichi00816@aws-0-ap-south-1.pooler.supabase.com:5432/postgres
+# 
